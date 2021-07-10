c598cad1f8197e869a5388206c39a04a1a82ecb3
Cleggatron <david.robert.clegg@gmail.com>
2021-07-10 21:10:54 +0100

    removed some unecessary comments

--- Assets/scripts.js
+++ Assets/scripts.js
@@ -1,11 +1,4 @@
 var APIKey = "9848bfb6d8989dcb1be0073be9867f89";
-
-var GlobalData;
-
-
-//var currentDayUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`
-//Seven Day API `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${APIKey}`
-
 var searchInputEl = document.getElementById("userInput");
 var searchButtonEl = document.getElementById("searchButton");
 
